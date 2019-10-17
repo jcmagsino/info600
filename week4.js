58e4281357c67fe773ead8479cd3f6a1db0234f2
jcmagsino <56407586+jcmagsino@users.noreply.github.com>
2019-10-17 14:56:21 -0400

    Update week4.js

--- week4.js
+++ week4.js
@@ -1,9 +1,9 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
-});
 
-const buttonTest = document.querySelector( button );
+    const buttonTest = document.querySelector( button );
 
-buttonTest.addEventListener('click', event => {
-  buttonTest.style.backgroundColor = "red";
+    buttonTest.addEventListener('click', event => {
+      buttonTest.style.backgroundColor = "red";
+    });
 });
