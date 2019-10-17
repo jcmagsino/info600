f8de2f5a81a5c318f5d5b1da4800f47d11763144
jcmagsino <56407586+jcmagsino@users.noreply.github.com>
2019-10-17 14:16:15 -0400

    Update week4.js

--- week4.js
+++ week4.js
@@ -1,8 +1,3 @@
-
-// This is a function to print a text depending on the button chosen in the HTML code
-function changeColor () {
-document.getElementById("red").style.backgroundColor = "red" };
-		
 const button = document.querySelector('button');
 
 button.addEventListener('click', event => {
