cb720d54934f631739a37d168050b158bb63ce1a
jcmagsino <56407586+jcmagsino@users.noreply.github.com>
2019-10-17 14:07:22 -0400

    Create INFO600 In-class Exercise-JosephCarlo-Magsino.js

---
+++ INFO600 In-class Exercise-JosephCarlo-Magsino.js
@@ -0,0 +1,5 @@
+
+// This is a function to print a text depending on the button chosen in the HTML code
+function changeColor () {
+document.getElementById("red").style.backgroundColor = "red" };
+		
